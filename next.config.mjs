dd2af6f59c3c2689a480197ee6433af545e78de6
Julio Gallardo <jfgallardo95@gmail.com>
2024-09-12 16:59:29 -0300

    cors

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,33 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   swcMinify: false,
-  source: "https://backend-barbershop.vercel.app/",
-  headers: [
-    { key: "Access-Control-Allow-Credentials", value: "true" },
-    { key: "Access-Control-Allow-Origin", value: "*" },
-    { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
-    { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
-  ]
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Access-Control-Allow-Credentials",
+            value: "true",
+          },
+          {
+            key: "Access-Control-Allow-Origin",
+            // Replace with your domain
+            value: "*",
+          },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value:
+              "X-CSRF-Token, X-Requested-With, Accept, Accept- Version, Content - Length, Content - MD5, Content - Type, Date, X - Api - Version",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
