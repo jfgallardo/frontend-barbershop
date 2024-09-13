9d314298b79a6948214f4dbf817c0b0d2349c6ac
Julio Gallardo <jfgallardo95@gmail.com>
2024-09-13 01:08:42 -0300

    api fix headers

--- src/data/hooks/useAPI.ts
+++ src/data/hooks/useAPI.ts
@@ -1,64 +1,72 @@
-import { useCallback } from 'react'
-import useSessao from './useSessao'
+import { useCallback } from "react";
+import useSessao from "./useSessao";
 
-const URL_BASE = process.env.NEXT_PUBLIC_URL_BASE
+const URL_BASE = process.env.NEXT_PUBLIC_URL_BASE;
 
 export default function useAPI() {
-    const { token } = useSessao()
+  const { token } = useSessao();
 
-    const httpGet = useCallback(
-        async function (uri: string): Promise<any> {
-            const path = uri.startsWith('/') ? uri : `/${uri}`
+  const httpGet = useCallback(
+    async function (uri: string): Promise<any> {
+      const path = uri.startsWith("/") ? uri : `/${uri}`;
 
-            const resp = await fetch(`${URL_BASE}${path}`, {
-                headers: {
-                    Authorization: `Bearer ${token}`,
-                },
-            })
-            return extrairDados(resp)
+      const resp = await fetch(`${URL_BASE}${path}`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+          "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Methods": "GET,OPTIONS,PATCH,DELETE,POST,PUT",
+          "Access-Control-Allow-Headers":
+            "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
         },
-        [token]
-    )
+      });
+      return extrairDados(resp);
+    },
+    [token]
+  );
 
-    const httpPost = useCallback(
-        async function (uri: string, body: any): Promise<any> {
-            const path = uri.startsWith('/') ? uri : `/${uri}`
-            const resp = await fetch(`${URL_BASE}${path}`, {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                    Authorization: `Bearer ${token}`,
-                },
-                body: JSON.stringify(body),
-            })
-            return extrairDados(resp)
+  const httpPost = useCallback(
+    async function (uri: string, body: any): Promise<any> {
+      const path = uri.startsWith("/") ? uri : `/${uri}`;
+      const resp = await fetch(`${URL_BASE}${path}`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+          "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Methods": "GET,OPTIONS,PATCH,DELETE,POST,PUT",
+          "Access-Control-Allow-Headers":
+            "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
         },
-        [token]
-    )
-    const httpDelete = useCallback(
-        async function (uri: string): Promise<any> {
-            const path = uri.startsWith('/') ? uri : `/${uri}`
-            const resp = await fetch(`${URL_BASE}${path}`, {
-                method: 'DELETE',
-                headers: {
-                    'Content-Type': 'application/json',
-                    Authorization: `Bearer ${token}`,
-                },
-            })
-            return extrairDados(resp)
+        body: JSON.stringify(body),
+      });
+      return extrairDados(resp);
+    },
+    [token]
+  );
+  const httpDelete = useCallback(
+    async function (uri: string): Promise<any> {
+      const path = uri.startsWith("/") ? uri : `/${uri}`;
+      const resp = await fetch(`${URL_BASE}${path}`, {
+        method: "DELETE",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
         },
-        [token]
-    )
+      });
+      return extrairDados(resp);
+    },
+    [token]
+  );
 
-    async function extrairDados(resp: Response) {
-        let conteudo = ''
-        try {
-            conteudo = await resp.text()
-            return JSON.parse(conteudo)
-        } catch (e) {
-            return conteudo
-        }
+  async function extrairDados(resp: Response) {
+    let conteudo = "";
+    try {
+      conteudo = await resp.text();
+      return JSON.parse(conteudo);
+    } catch (e) {
+      return conteudo;
     }
+  }
 
-    return { httpGet, httpPost, httpDelete }
+  return { httpGet, httpPost, httpDelete };
 }
